953aaaa4efa46d1bb0e6034fce68f9d3008abb32
Zohaib Manzoor <zohaibmanzoor2004@gmail.com>
2025-07-08 03:53:58 +0500

    Resolved the Errors,No page found,Schema Vaidations(joi) and etc.

--- app.js
+++ app.js
@@ -2,14 +2,21 @@ const express = require("express");
 const app = express();
 const mongoose= require("mongoose");//db
 const Listing = require("./models/listing.js");
+const {listingSchema} = require("./Schema.js");
 const path = require("path");//
 app.use(express.urlencoded({extended: true}));
-//Acquiring database
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+//Acquiring database 
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
 const methodOverride = require("method-override");
 app.use(methodOverride("_method"));
 const ejsMate=require("ejs-mate");
+const { nextTick } = require("process");
 app.engine("ejs", ejsMate);
+//Views Setup
+app.set("view engine", "ejs");
+app.set("views",path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"/public")));
 app.use(express.static(path.join(__dirname, 'sources')));
 async function main(){
@@ -27,6 +34,16 @@ let port = 8080;
 app.listen( port , ()=>{
     console.log(" listening at port 8080 server");
 }); 
+const validateListing = (req, res, next ) => {
+    let {error } = listingSchema.validate(req.body);
+    if (error){
+        let errMsg= error.details.map((el) => el.message).join(",");
+        //throw ExpressError(404, result.error);
+        throw new ExpressError(404, errMsg);
+    }else{
+        next();
+    }
+}
 
 app.get("/", (req,res)=>{
     res.send("Hello i am the root Route");
@@ -44,40 +61,56 @@ app.get("/listings/new",(req,res)=>{
     res.render("listings/new.ejs");
 })
 //Show Route
-app.get("/listings/:id" ,async (req,res)=>{
+app.get("/listings/:id" ,
+    wrapAsync(async (req,res)=>{
     let {id}=req.params;
-    console.log(id);
+    //console.log(id);
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs",{listing});
-});
-
+    })
+);
 //Post the New Listing 
-app.post("/listings", async(req,res)=>{
+//OR Create Route
+//here we can remove wrapAsync
+app.post("/listings", validateListing,
+    wrapAsync(async (req,res,next)=>{
     let newListing=req.body.listing;
+    //condition to check if the body is not null and deal the error
     //console.log("Image URL: ", newListing.image.url);
     if (newListing.image.url === ""){
         let DefaultURL= "https://images.unsplash.com/photo-1499793983690-e29da59ef1c2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2glMjBob3VzZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60";
         newListing.image.url = DefaultURL;
-        console.log("NEW LISTING Object DATA: ", newListing.image.url);
-        newListing = new Listing(newListing);
-        await newListing.save();
-        console.log("Listing was inserted: ");
-        res.redirect("/listings");
     }
-});
+    let result = listingSchema.validate(req.body);
+    console.log(result);
+    
+    if (result.error){
+        throw new ExpressError(400, result.error); // optional: better error
+}
+
+    //console.log("NEW LISTING Object DATA: ", newListing.image.url);
+    newListing = new Listing(newListing);
+    await newListing.save();
+    //console.log("Listing was inserted: ");
+    res.redirect("/listings");  
+    }
+));
 
 //Edit Route
-app.get("/listing/:id/edit",async(req,res)=>{
+app.get("/listing/:id/edit",
+    wrapAsync(async(req,res)=>{
     let {id} = req.params;
     //Ok till here
     const listing = await Listing.findById(id);
-    console.log("Request reached at edit route with listing: ", listing);
+    //console.log("Request reached at edit route with listing: ", listing);
     res.render("listings/edit.ejs",{listing});
-})
+    })
+);
 
-//Post/Put the Edited Route
+//Post/Put the Edited Route//Update Route
 
-app.put("/listing/:id/edit",async (req,res)=>{
+app.put("/listing/:id/edit",validateListing, 
+    wrapAsync(async (req,res)=>{
     let {id} = req.params;
     id = id.trim();
     //console.log(req.body.listing);
@@ -87,19 +120,31 @@ app.put("/listing/:id/edit",async (req,res)=>{
     //let editedListing = req.body.listing;
     //console.log(editedListing);
     res.redirect("/listings");
-});
+    })
+);
 
 //Delete Route
-app.delete("/listing/:id/delete",async(req,res)=>{
+app.delete("/listing/:id/delete",
+    wrapAsync(async(req,res)=>{
     //console.log("reached at delete");
     let {id} = req.params;
     let DelListing = await Listing.findByIdAndDelete(id);
     //console.log("The Listing was Deleted: ",DelListing);
     res.redirect("/listings");
+    }
+));
+
+//Page unmatched default response (page not found)
+app.all("*",(req,res,next)=>{
+    next(new ExpressError(404, "Page not Found!"));
+})
+
+app.use((err,req,res,next)=>{
+    let {statusCode=500, message="Something went wrong" } = err;
+    res.render("error.ejs",{err});
+    //res.status(500).send("Soemthing went wrong while Saving Data to Database");
+    //res.status(statusCode).send(message);
 });
-//Views Setup
-app.set("view engine", "ejs");
-app.set("views",path.join(__dirname,"views"));
 // app.get("/testListing" ,async (req,res)=>{
 //     let sampleListing = new Listing({ 
 //         title: "My new Villa",
